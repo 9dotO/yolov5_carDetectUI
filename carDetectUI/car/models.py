05bbd5d4d54b9a4aab3755bbe39e8c1460ed474c
9dotO <kgl04120@gmail.com>
2023-10-20 11:30:58 +0900

    완성

--- carDetectUI/car/models.py
+++ carDetectUI/car/models.py
@@ -8,7 +8,11 @@ from django.utils.translation import gettext_lazy as _
 
 class ImageModel(models.Model):
     image = models.ImageField(_("image"), upload_to='images')
-    result_image = models.ImageField(_("Result Image"),  blank=True, null=True)
+    result_image = models.ImageField(_("Result Image"), upload_to='results', blank=True, null=True)
+    
+    detected_classes = models.JSONField(null=True, blank=True)
+    class_counts = models.JSONField(null=True, blank=True)
+    
     class Meta:
         verbose_name = "Image"
         verbose_name_plural = "Images"
@@ -19,3 +23,7 @@ class ImageModel(models.Model):
 
 
     
+
+class VideoModel(models.Model):
+    title = models.CharField(max_length=100)
+    video = models.FileField(upload_to='videos/')
